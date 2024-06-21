842ff626268c53693c54843da19ec016d2b58523
My Chiffon Nguyen <chiffonng136@gmail.com>
2024-06-21 13:06:56 +0200

    04_primitive_types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,15 +5,16 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// https://doc.rust-lang.org/book/ch03-02-data-types.html
 
 fn main() {
-    let a = ???
+    // let a = [1..=100]; // an array of 100 elements, from 1 to 100
+    let a = [1..101]; // an array of 100 elements, from 1 to 100; exclude 101
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
-        panic!("Array not big enough, more elements needed")
+        panic!("Array not big enough, more elements needed") // raise runtime error
     }
 }
