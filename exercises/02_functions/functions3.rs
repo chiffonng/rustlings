5b75b757a951316c97ffb8b24c998e071fde5e7d
My Chiffon Nguyen <chiffonng136@gmail.com>
2024-06-21 12:25:50 +0200

    02_functions

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(3); // add 3 as an argument to call_me
 }
 
 fn call_me(num: u32) {
