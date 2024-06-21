a2db08149a0ae6a4e1fc1a740f64f3022cc1794c
My Chiffon Nguyen <chiffonng136@gmail.com>
2024-06-21 12:25:35 +0200

    01_variables

--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -3,9 +3,9 @@
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
 
 fn main() {
-    let x: i32;
+    let x: i32 = 3; // assign a value of data type i32 to x; i32 is a 32-bit signed integer
     println!("Number {}", x);
 }
